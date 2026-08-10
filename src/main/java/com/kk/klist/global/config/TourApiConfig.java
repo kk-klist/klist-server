5fdf97dc10cb88a93d7d4d1059f4699bfd7f0ac7
wonajin <ajinwon1@gmail.com>
2026-08-10 10:32:19 +0900

    chore: 지도 도메인 준비 (build.gradle + RestClient config + API 키) (#23)

---
+++ src/main/java/com/kk/klist/global/config/TourApiConfig.java
@@ -0,0 +1,27 @@
+package com.kk.klist.global.config;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class TourApiConfig {
+
+    private static final Duration CONNECT_TIMEOUT = Duration.ofSeconds(3);
+    private static final Duration READ_TIMEOUT = Duration.ofSeconds(5);
+
+    @Bean
+    public RestClient tourRestClient(@Value("${tour.api.base-url}") String baseUrl) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(CONNECT_TIMEOUT);
+        requestFactory.setReadTimeout(READ_TIMEOUT);
+
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .requestFactory(requestFactory)
+                .build();
+    }
+}
